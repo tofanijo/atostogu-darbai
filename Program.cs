f4296df0356e50693a06b87f65d49c38ba0f51ee
tofanijo <tofanijo@gmail.com>
2019-12-27 19:04:48 +0200

    Leson 2 uzduotis 1

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_2_uzduotis_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string VARDAS = "";
+            string PAVARDE = "";
+            Console.WriteLine("Iveskite savo Varda:");
+            VARDAS = Console.ReadLine();
+            Console.WriteLine("Iveskite savo Pavarde:");
+            PAVARDE = Console.ReadLine();
+
+            Console.Write("Jusu Vardas Pavarde: {0}_{1}",  VARDAS, PAVARDE);
+            Console.ReadLine();
+
+        }
+    }
+}
