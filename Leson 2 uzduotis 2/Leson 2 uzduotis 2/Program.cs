fc9ac3b6dc979f11a62dfc7164b727388febf4f2
tofanijo <tofanijo@gmail.com>
2019-12-28 11:30:14 +0200

    Leson 2 uzduotis 2

---
+++ Leson 2 uzduotis 2/Leson 2 uzduotis 2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leson_2_uzduotis_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Paprašyti vartotojo įvesti apskritimo spindulį. 
+            //Išvesti apskritimo ilgį ir plotą.
+            //Pastaba: naudoti "pi" skaičiui naudoti Math.PI
+
+            Console.WriteLine("Iveskite apskritimo spinduli:");
+            double apskritimoDiametras = Convert.ToDouble(Console.ReadLine());
+            double apskritimoSpindulys = apskritimoDiametras / 2;
+            
+
+            double apskritimoIlgis = 2 * Math.PI * (apskritimoSpindulys);
+            Console.WriteLine("Apskritimo ilgis: {0}", apskritimoIlgis);
+            double apskritimoPlotas = Math.PI * Math.Sqrt(apskritimoSpindulys);
+            Console.WriteLine("Apskritimo plotas: {0}", apskritimoPlotas);
+            Console.ReadLine();
+
+
+        }
+    }
+}
