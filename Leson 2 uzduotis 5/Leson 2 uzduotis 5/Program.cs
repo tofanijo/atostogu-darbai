16856141e06de8e6ca134bc7d6537f05ce64cb05
tofanijo <tofanijo@gmail.com>
2019-12-30 09:25:16 +0200

    Leson 2 uzduotis 5

---
+++ Leson 2 uzduotis 5/Leson 2 uzduotis 5/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leson_2_uzduotis_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Paprašyti vartotojo įvesti 2 stačiakampio kraštinių ilgį: x ir y
+            //Parodyti kiek pilnų stačiakampių, kurių kraštinių ilgis lygus 2x3, galima į jį įrašyti. 
+
+            double krastineX;
+            double krastineY;
+
+            Console.WriteLine("Iveskite staciakampio krastine x:");
+            krastineX = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Iveskite staciakampio krastine y:");
+            krastineY = Convert.ToDouble(Console.ReadLine());
+
+
+            Console.WriteLine(+(krastineX / 2) = );
+        }
+    }
+}
