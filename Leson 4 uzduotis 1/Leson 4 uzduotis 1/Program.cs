4320290cd0df05d65a8f1096f2af77450f791ca1
tofanijo <tofanijo@gmail.com>
2020-01-02 19:14:00 +0200

    Leson 4 uzduotis 1

---
+++ Leson 4 uzduotis 1/Leson 4 uzduotis 1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leson_4_uzduotis_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Isvesti savo varda 10 kart naudojant for cikla.
+
+            string vardas = "Romanas   ";
+            for (int i = 0; i < vardas.Length;i++)
+                Console.WriteLine("Romanas {0}", i);
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ Leson 4 uzduotis 2/Leson 4 uzduotis 2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leson_4_uzduotis_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Parasyti programa kuri isveda skaicius nuo 0 iki 10
+
+            string skaicius = "";
+            for (int i = 0; i < 11;i++)
+
+            
+                Console.WriteLine("skaicius {0}", i);
+                Console.ReadLine();
+            }
+            
+        }
+    }
+
+
