31500ba9cdeb703d93d814ae29f95eaac9079560
tofanijo <tofanijo@gmail.com>
2020-01-02 20:38:02 +0200

    Leson 3 uzduotis 4

---
+++ Leson 3 uzduotis 4/Leson 3 uzduotis 4/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leson_3_uzduotis_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Išvesti skaičius nuo 0 iki 100, kurie dalinasi iš 3 be liekanos.
+
+
+
+            for (int i = 0; i <= 100; i++)
+            {
+
+                if ((i % 3) == 0)
+
+                    Console.Write(i);
+
+                Console.ReadLine();
+            }
+            
+        }
+    }
+}
