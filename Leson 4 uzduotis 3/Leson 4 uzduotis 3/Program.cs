1ae5dbbd6fc568da374b60d333f2d09d4a99bbd9
tofanijo <tofanijo@gmail.com>
2020-01-02 20:09:19 +0200

    Leson 4 uzduotis 3

---
+++ Leson 4 uzduotis 3/Leson 4 uzduotis 3/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leson_4_uzduotis_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Parodyti skaiciu nuo 10 iki 20 suma.
+
+            
+            int suma = 0;
+
+
+            for (int i = 10; i < 20; i++)
+            {
+                suma += i;
+            }
+            
+
+
+          
+                { 
+                    Console.WriteLine("Skaiciu nuo 10 iki 20 suma = {0}", suma);
+                }
+            Console.ReadLine();
+            }
+        }
+
+    }
