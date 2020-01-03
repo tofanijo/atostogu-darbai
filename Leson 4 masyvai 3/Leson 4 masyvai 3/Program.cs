7e78de07aad589efc9138bdb0c989fdb52b45912
tofanijo <tofanijo@gmail.com>
2020-01-03 22:28:01 +0200

    Leson 4 masyvai 3

---
+++ Leson 4 masyvai 3/Leson 4 masyvai 3/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leson_4_masyvai_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Išvesti masyvo iš 1 - os užduoties elementų sumą
+
+            int[] masyvas1 = { 1, 2, 3, 4, 5 };
+            int suma = 0;
+
+            for (int i = 0; i < 5; i++)
+            {
+                suma += masyvas1[i];
+            }
+                
+
+                    Console.WriteLine(suma);
+                
+                Console.ReadLine();
+            }
+        }
+    }
+
