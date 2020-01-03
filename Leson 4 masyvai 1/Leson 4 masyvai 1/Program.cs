70dd13380740e6fd0200dfefc8b760cb2a4503a0
tofanijo <tofanijo@gmail.com>
2020-01-03 21:54:40 +0200

    Leson_4_masyvai_1

--- Leson 4 masyvai 1/Leson 4 masyvai 1/Program.cs
+++ Leson 4 masyvai 1/Leson 4 masyvai 1/Program.cs
@@ -10,6 +10,15 @@ namespace Leson_4_masyvai_1
     {
         static void Main(string[] args)
         {
+            //Sukurti masyvą iš 5 int elementų ir išvesti jį naudojant for ciklą
+            int[] masyvas1 = {1,2,3,4,5};
+            
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine(masyvas1[i]);
+            }
+            Console.ReadLine();
         }
     }
 }
