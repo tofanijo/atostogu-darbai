9a977a5c84b351106b49e315d51f82e08b2b248f
tofanijo <tofanijo@gmail.com>
2020-01-03 11:15:36 +0200

    Leson 4 uzduotis 4

---
+++ Leson 4 uzduotis 4/Leson 4 uzduotis 4/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace Leson_4_uzduotis_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+            string[] trikampis =
+            {
+
+
+                "             *",
+                "            ***",
+                "           *****",
+                "          *******",
+               "         *********",
+               "        ************",
+              "       **************",
+            };
+
+            for (int i = 0; i < trikampis.Length; i++)
+            {
+                Console.WriteLine(trikampis[i]);
+
+            }
+            Console.ReadLine();
+        }
+    }
+
+}
